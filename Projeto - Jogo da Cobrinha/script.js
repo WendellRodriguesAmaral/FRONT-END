8395b2fb968025f0ecaa6098e1c1343062f8bf12
WendellRodrigues <42788841+WendellRodriguesAmaral@users.noreply.github.com>
2020-11-11 17:49:23 -0300

    alterações no js
    criamos a função q inicia o jogo e aumenta e diminui o tamanho dela

--- Projeto - Jogo da Cobrinha/script.js
+++ Projeto - Jogo da Cobrinha/script.js
@@ -6,6 +6,8 @@ snake[0]={
     x:8*box,
     y:8*box
 }
+let direction = "right"
+
 
 
 
@@ -23,6 +25,28 @@ function criarCorbrinha(){
 }
 
 
-criarBG();
-criarCorbrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCorbrinha();
+    let snakeX=snake[0].x
+    let snakeY=snake[0].y
+
+    if(direction=="right") snakeX+=box;
+    if(direction=="left") snakeX -=box;
+    if(direction=="up") snakeY-= box;
+    if(direction=="down") snakeY+=box;
+
+    snake.pop();
+
+    let newHead={
+        x:snakeX,
+        y:snakeY
+    }
+
+    snake.unshift(newHead);//o metodo unshift add o elemento a posição 0 de um array
+    
+}
+
+let jogo= setInterval(iniciarJogo,100) //"atualizar" a cada 100ms
+
 
