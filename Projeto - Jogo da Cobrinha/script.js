eea3d9ad979f6edbb64166f02ff5bb1d68042a91
WendellRodrigues <42788841+WendellRodriguesAmaral@users.noreply.github.com>
2020-11-11 19:16:27 -0300

    criação da função de movimento da cobrinha

--- Projeto - Jogo da Cobrinha/script.js
+++ Projeto - Jogo da Cobrinha/script.js
@@ -25,7 +25,20 @@ function criarCorbrinha(){
 }
 
 
+document.addEventListener("keydown",update); // esperando o evento de apertar a tecla
+
+function update(event){
+    if(event.keyCode ==37 && direction != "right") direction="left";
+    if(event.keyCode==38 && direction != "down") direction="up";
+    if(event.keyCode==39 && direction != "left") direction="right"
+    if(event.keyCode==40 && direction != "up") direction="down"
+
+
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15*box && direction=="right") snake[0].x =0; // se a posição horizontal dela passar de 480px, sua posição volta a ser 0
+    
     criarBG();
     criarCorbrinha();
     let snakeX=snake[0].x
@@ -44,7 +57,7 @@ function iniciarJogo(){
     }
 
     snake.unshift(newHead);//o metodo unshift add o elemento a posição 0 de um array
-    
+
 }
 
 let jogo= setInterval(iniciarJogo,100) //"atualizar" a cada 100ms
